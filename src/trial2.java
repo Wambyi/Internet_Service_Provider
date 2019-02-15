0324c1e665ee122910ba75c1d0f44cbf8994e82c
Gladys Daniel <gladysdaniel@gladyss-mbp.wireless.concordia.ca>
2019-02-15 17:13:05 -0500

    Initial commit

---
+++ src/trial2.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class trial2 {
+
+
+
+    public static void main (String [] args)
+    {
+        operators func = new operators();
+
+        func.selectPack();
+        func.getHours();
+
+
+
+    }
+
+
+
+
+}
